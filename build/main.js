a3a1daf9108f079453c8e2d41e1a4bb200df3430
theMugician <gslonina7@gmail.com>
2020-04-25 22:28:56 -0700

    add transitions and ripple animations

--- build/main.js
+++ build/main.js
@@ -2,17 +2,32 @@ let active = 'home';
 
 const buttons = document.querySelectorAll('.button');
 
-function update() { 
-	console.log(active);
-  let clicked = this.className;
-  clicked = clicked.split('--').pop();
-  console.log(clicked);
+function handler(ev) {
+
+	// get clicked button
+  let clicked = this.className
+					 					.split(' ')[1]
+						 				.split('--')
+							 			.pop();
+
+  // ripple effect
+  let x = ev.clientX;
+  let y = ev.clientY;
+  let ripple = `.ripple--${clicked}`;
+  let node = document.querySelector(ripple);
+  let newNode = node.cloneNode(true);
+  newNode.classList.add('animate');
+  newNode.style.left = ev.clientX - 5 + "px";
+  newNode.style.top = ev.clientY - 5 + "px";
+  node.parentNode.replaceChild(newNode, node);
+
+  // animate new button
   if ( clicked !==  active ) {
   	document.querySelector(`.button--${active}`).classList.remove('active');
   	this.classList.add('active');
   	active = clicked;
   }
-  //document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
 }
 
-buttons.forEach(button => button.addEventListener('click', update));
+buttons.forEach(button => button.addEventListener('click', handler));
